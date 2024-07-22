ea98277c6b808564fe3a311d0eb99dde3142352b
Aleksandra Zueva <aleksandra.zueva@exness.com>
2024-07-22 09:02:10 +0300

    add onClickListener

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        searchButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Поиск", Toast.LENGTH_SHORT).show()
+        }
+
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Медиатека", Toast.LENGTH_SHORT).show()
+
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        val settingsListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на Настройки", Toast.LENGTH_SHORT).show()
+            }
+        }
+        settingsButton.setOnClickListener(settingsListener)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
