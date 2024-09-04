458e3d2dea73e98ee6f2dc4420abf002e1d9d905
Aleksandra Zueva <aleksandra.zueva@exness.com>
2024-09-04 18:55:24 +0300

    add retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.glide)
+    implementation(libs.retrofit)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
