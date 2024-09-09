7a470038d4a0a9f103abaea6dee8b84dbf848a25
Aleksandra Zueva <aleksandra.zueva@exness.com>
2024-09-09 21:40:15 +0300

    add logger

--- app/src/main/java/com/example/playlistmaker/network/RetrofitClient.kt
+++ app/src/main/java/com/example/playlistmaker/network/RetrofitClient.kt
@@ -1,15 +1,22 @@
 package com.example.playlistmaker.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+        .build()
+
     private const val BASE_URL = "https://itunes.apple.com/"
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val iTunesService = retrofit.create(ITunesService::class.java)
+    val iTunesService: ITunesService = retrofit.create(ITunesService::class.java)
 }
