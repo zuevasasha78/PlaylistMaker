4dc64f452ee575b1199a603f32c5264c7a510736
Aleksandra Zueva <aleksandra.zueva@exness.com>
2024-09-11 20:09:42 +0300

    add trackId

--- app/src/main/java/com/example/playlistmaker/network/data/TrackListResponse.kt
+++ app/src/main/java/com/example/playlistmaker/network/data/TrackListResponse.kt
@@ -6,5 +6,9 @@ data class TrackListResponse(
 )
 
 data class Track(
-    val trackName: String, val artistName: String, val trackTimeMillis: Int?, val artworkUrl100: String
+    val trackId: Int,
+    val trackName: String,
+    val artistName: String,
+    val trackTimeMillis: Int?,
+    val artworkUrl100: String
 )
