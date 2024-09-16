1ef5c619ba9474fcf331c50f5ff5c4e09aa4b759
Aleksandra Zueva <aleksandra.zueva@exness.com>
2024-09-16 23:39:04 +0300

    add uploadImage

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -1,19 +1,30 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.SearchActivity.Companion.TRACK_DATA
+import com.example.playlistmaker.network.data.Track
 
 class AudioPlayerActivity : AppCompatActivity() {
+
+    private lateinit var track: Track
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_audioplayer)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val trackImage = findViewById<ImageView>(R.id.trackImage)
+
+        track = stringToTrackList(intent.getStringExtra(TRACK_DATA))!!
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.audioplayer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -24,6 +35,8 @@ class AudioPlayerActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
+
+        uploadImage(trackImage)
     }
 
     override fun onResume() {
@@ -35,4 +48,19 @@ class AudioPlayerActivity : AppCompatActivity() {
         super.onStop()
         // todo для фонового режима???
     }
+
+    private fun uploadImage(trackImage: ImageView) {
+        val roundValue = 8
+        Glide.with(this)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .transform(
+                FitCenter(),
+                RoundedCorners(
+                    roundValue * (resources.displayMetrics.density).toInt()
+                )
+            )
+            .into(trackImage)
+    }
+
 }
