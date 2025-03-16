4cd97b7a7a850818ed54b611032e27626b4581d2
Aleksandra Zueva <aleksandra.zueva@exness.com>
2025-03-16 15:18:02 +0200

    add TrackAdapter

---
+++ app/src/main/java/com/example/playlistmaker/presentation/ui/tracks/TrackAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.playlistmaker.presentation.ui.tracks
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.domain.models.Track
+
+class TrackAdapter(private val clickListener: TrackClickListener) :
+    RecyclerView.Adapter<TrackViewHolder>() {
+
+    private var trackItems: MutableList<Track> = mutableListOf()
+
+    fun setItems(tracks: MutableList<Track>) {
+        trackItems = tracks
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        return TrackViewHolder.from(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return trackItems.size
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        trackItems.getOrNull(position)?.let { track ->
+            holder.bind(track)
+            holder.itemView.setOnClickListener { clickListener.onTrackClick(track) }
+        }
+    }
+
+    fun interface TrackClickListener {
+        fun onTrackClick(track: Track)
+    }
+}
