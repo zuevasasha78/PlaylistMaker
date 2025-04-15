cbbeb2201977c6db4fcaa910ecdededecae6aafc
aleksandra.zueva <aleksandra.zueva@exness.com>
2025-04-15 21:13:56 +0300

    search data di

--- app/src/main/java/com/example/playlistmaker/mvvm/search/data/di/SearchDataModule.kt
+++ app/src/main/java/com/example/playlistmaker/mvvm/search/data/di/SearchDataModule.kt
@@ -2,11 +2,16 @@ package com.example.playlistmaker.mvvm.search.data.di
 
 import com.example.playlistmaker.mvvm.search.data.impl.TracksHistoryRepositoryImpl
 import com.example.playlistmaker.mvvm.search.data.impl.TracksRepositoryImpl
+import com.example.playlistmaker.mvvm.search.data.network.ITunesService
 import com.example.playlistmaker.mvvm.search.data.network.NetworkClient
 import com.example.playlistmaker.mvvm.search.data.network.RetrofitNetworkClient
 import com.example.playlistmaker.mvvm.search.domain.api.TracksHistoryRepository
 import com.example.playlistmaker.mvvm.search.domain.api.TracksRepository
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val searchDataModule = module {
 
@@ -19,6 +24,25 @@ val searchDataModule = module {
     }
 
     single<NetworkClient> {
-        RetrofitNetworkClient()
+        RetrofitNetworkClient(get())
     }
+
+    single {
+        OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com/")
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single { get<Retrofit>().create(ITunesService::class.java) }
 }
