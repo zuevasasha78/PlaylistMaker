0b58b7a13e11469dca4679e26c06d8715c0e8899
aleksandra.zueva <aleksandra.zueva@exness.com>
2025-06-05 18:30:50 +0300

    Parcelize Track

--- app/src/main/java/com/example/playlistmaker/mvvm/search/domain/models/Track.kt
+++ app/src/main/java/com/example/playlistmaker/mvvm/search/domain/models/Track.kt
@@ -1,5 +1,9 @@
 package com.example.playlistmaker.mvvm.search.domain.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Track(
     val trackId: Int,
     val trackName: String,
@@ -11,4 +15,4 @@ data class Track(
     val primaryGenreName: String,
     val country: String,
     val previewUrl: String,
-)
+) : Parcelable
