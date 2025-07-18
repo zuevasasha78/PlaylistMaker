8a9111d4dba6c3cf238f16b83fc835916dd60da4
aleksandra.zueva <aleksandra.zueva@exness.com>
2025-07-18 20:30:13 +0300

    replace AppDatabase on TrackDao

--- app/src/main/java/com/example/playlistmaker/mvvm/db/data/FavoriteTracksRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/mvvm/db/data/FavoriteTracksRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker.mvvm.db.data
 
 import com.example.playlistmaker.mvvm.db.data.converters.TrackDbConvertor
+import com.example.playlistmaker.mvvm.db.data.dao.TrackDao
 import com.example.playlistmaker.mvvm.db.data.entity.TrackEntity
 import com.example.playlistmaker.mvvm.db.domain.FavoriteTracksRepository
 import com.example.playlistmaker.mvvm.search.domain.models.Track
@@ -8,27 +9,27 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class FavoriteTracksRepositoryImpl(
-    private val appDatabase: AppDatabase,
+    private val trackDao: TrackDao,
     private val trackDbConvertor: TrackDbConvertor,
 ) : FavoriteTracksRepository {
 
     override fun getFavoriteTracks(): Flow<List<Track>> = flow {
-        val trackEntity = appDatabase.getTrackDao().getTracks()
+        val trackEntity = trackDao.getTracks()
         emit(convertFromTrackEntity(trackEntity))
     }
 
     override suspend fun setFavoriteTracks(track: Track) {
-        if (appDatabase.getTrackDao().getTrackById(track.trackId) == null) {
-            appDatabase.getTrackDao().insertTrack(trackDbConvertor.map(track))
+        if (trackDao.getTrackById(track.trackId) == null) {
+            trackDao.insertTrack(trackDbConvertor.map(track))
         }
     }
 
     override suspend fun deleteFavoriteTracks(trackId: Long) {
-        appDatabase.getTrackDao().deleteTrack(trackId)
+        trackDao.deleteTrack(trackId)
     }
 
     override suspend fun getFavoriteTrackById(trackId: Long): Track? {
-        val trackEntity = appDatabase.getTrackDao().getTrackById(trackId)
+        val trackEntity = trackDao.getTrackById(trackId)
         return if (trackEntity != null) {
             trackDbConvertor.map(trackEntity, isFavorite = true)
         } else {
