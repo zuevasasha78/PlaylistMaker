0afb392c61df7d549a1503cae54c2db8ca0ef31f
aleksandra.zueva <aleksandra.zueva@exness.com>
2025-08-11 11:30:36 +0300

    fix xml data

--- app/src/main/java/com/example/playlistmaker/mvvm/playlist/ui/fragment/PlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/mvvm/playlist/ui/fragment/PlaylistFragment.kt
@@ -14,11 +14,14 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentPlaylistBinding
+import com.example.playlistmaker.mvvm.library.domain.models.Playlist
 import com.example.playlistmaker.mvvm.playlist.ui.view_modal.PlaylistViewModel
+import com.example.playlistmaker.mvvm.search.domain.models.Track
 import com.example.playlistmaker.mvvm.search.ui.fragment.TrackAdapter
-import com.example.playlistmaker.utils.durationFormat
+import com.example.playlistmaker.utils.convertMinAndSecToLong
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
+import com.google.gson.Gson
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -47,6 +50,11 @@ class PlaylistFragment : Fragment() {
         setData()
     }
 
+    override fun onDestroyView() {
+        _viewBinding = null
+        super.onDestroyView()
+    }
+
     private fun initBottomSheet() {
         bottomSheetBehavior = BottomSheetBehavior.from(viewBinding.tracklistBottomSheet)
         bottomSheetBehavior.state = STATE_COLLAPSED
@@ -54,17 +62,7 @@ class PlaylistFragment : Fragment() {
 
     private fun setData() {
         viewModel.playlistLiveData.observe(viewLifecycleOwner) { playlist ->
-            Glide.with(requireContext())
-                .load(playlist.coverImageUrl)
-                .placeholder(R.drawable.placeholder)
-                .transform(
-                    CenterCrop(),
-                )
-                .into(viewBinding.coverImage)
-            viewBinding.playlistName.text = playlist.name
-            viewBinding.playlistDescription.text = playlist.description
-            viewBinding.playlistDuration.text = durationFormat(playlist.tracksAmount * 30 * 1_000)
-            viewBinding.trackAmount.text = playlist.tracksAmount.toString()
+            setPlaylistData(playlist)
 
             if (playlist.tracksAmount != 0) {
                 initBottomSheet()
@@ -75,9 +73,33 @@ class PlaylistFragment : Fragment() {
         }
     }
 
+    private fun setPlaylistData(playlist: Playlist) {
+        Glide.with(requireContext())
+            .load(playlist.coverImageUrl)
+            .placeholder(R.drawable.placeholder)
+            .transform(
+                CenterCrop(),
+            )
+            .into(viewBinding.coverImage)
+        viewBinding.playlistName.text = playlist.name
+        viewBinding.playlistDescription.text = playlist.description
+        setPlaylistDuration(0)
+        viewBinding.trackAmount.text = requireContext().resources.getQuantityString(
+            R.plurals.track_amount,
+            playlist.tracksAmount,
+            playlist.tracksAmount
+        )
+    }
+
     private fun setTracksListData() {
         viewModel.tracksListLiveData.observe(viewLifecycleOwner) { tracksList ->
+            val playlistDuration = (tracksList.sumOf {
+                convertMinAndSecToLong(it.trackTimeMillis)
+            } / 1000 / 60).toInt()
+            setPlaylistDuration(playlistDuration)
+
             val trackAdapter = TrackAdapter { track ->
+                openAudioPlayer(track)
             }
             trackAdapter.setItems(tracksList)
             val layoutManager = LinearLayoutManager(
@@ -91,14 +113,24 @@ class PlaylistFragment : Fragment() {
         }
     }
 
+    private fun setPlaylistDuration(duration: Int) {
+        viewBinding.playlistDuration.text = requireContext().resources.getQuantityString(
+            R.plurals.minute_amount,
+            duration,
+            duration
+        )
+    }
+
+    private fun openAudioPlayer(track: Track) {
+        val trackJson = Gson().toJson(track)
+        val action =
+            PlaylistFragmentDirections.actionPlaylistFragmentToAudioPlayerFragment(trackJson)
+        findNavController().navigate(action)
+    }
+
     private fun setOnBackButtonListener() {
         viewBinding.toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
-    }
-
-    override fun onDestroyView() {
-        _viewBinding = null
-        super.onDestroyView()
     }
 }
